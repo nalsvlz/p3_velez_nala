c612628e57c31cf95baad7610860f1f5000b2ee1
nalsvlz <81934274+nalsvlz@users.noreply.github.com>
2021-04-30 23:22:55 -0400

    Add files via upload

---
+++ p3-velez-nala/form/default.inc.php
@@ -0,0 +1,16 @@
+<style type="text/css">
+  h1 {
+    font-size: 50px;
+    text-align: center;
+    font-family: 'Josefin Sans', sans-serif;
+  }
+
+  p {
+    font-size: 20px;
+    text-align: center;
+    font-family: 'Josefin Sans', sans-serif;
+  }
+</style>
+
+<h1>Hello</h1>
+  <p>Form processor by <a href="http://www.nvdesignagency.com">NV Design Agency</a>.</p>
---
+++ p3-velez-nala/form/thankyou.inc.php
@@ -0,0 +1,18 @@
+<style type="text/css">
+  h1 {
+    font-size: 50px;
+    text-align: center;
+    font-family: 'Josefin Sans', sans-serif;
+  }
+
+  p {
+    font-size: 20px;
+    text-align: center;
+    font-family: 'Josefin Sans', sans-serif;
+  }
+</style>
+
+<div class="container">
+  <h1>Thank you for your enquiry</h1>
+  <p>Thanks for your interest. we have received your message, and we will be in touch shortly.</p>
+</div>
